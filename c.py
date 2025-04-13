1f716a149bb929a5680fbdcc611a8884c5f5f52a
ydk1191120641 <66811213+ydk1191120641@users.noreply.github.com>
2025-04-13 15:27:21 +0900

    Add files via upload

---
+++ c.py
@@ -0,0 +1,431 @@
+from time import sleep
+import requests
+from datetime import datetime
+import pytz
+import paramiko
+from concurrent.futures import ThreadPoolExecutor
+import queue
+import time
+
+walletsnum = {'0x06bd5eb9645A0733F8d2027A78a779fa6C42b08e':0}
+
+# 钱包地址数组
+wallets = [
+    '0x06bd5eb9645A0733F8d2027A78a779fa6C42b08e',
+    '0x65c8E6E1c443554fA1F385bf6CE4495ba1eaFA3c',
+    '0xE8D974cB4bD6b604cD1e952deE8B57ed22E4230A',
+    '0xF43D6dE32a71C2A561CEE5d758304cfb12684e2d',
+    '0xbAFB1fe4941431f4b39d8C6BCbC18D14e6512fC6',
+    '0x58344fFc973FB97B54542f652B281dFf631C88A3',
+    '0x999Db68c0DB29Ad5C8f24912AF68FD18D5Cffa1B',
+    '0x0fa27987D02fBC5263879a1b5618459f71f7947d',
+    '0xCd9f9F1243ea82ae304f6f23C79A7A0A88506Cf1',
+    '0x2b9518721a0035a2B78759cFB51bEe1b1480c203',
+    '0x61D65d3A6e72CaD5f0FbA4C596B347733033Ebc6',
+    '0xC7521d67e2D7d6258C4EbE013AcCF54d3bba7212',
+    '0x031AD236938a9d68a36B7B611B87CA6EaB02acA8',
+    '0xf438ca2976Ab65A932C1c036a2D9edc10408bFFe',
+    '0x99b03AF1bad5Fae85C646594900aB06c5b1efB0f',
+    '0x082c34914119B7cBDe3A72C60D90A4fA5CFFF61B',
+    '0xe5aDBBc371a4cAE8CA0947072daA56c8eAB6e467',
+    '0xbfF33B4C1EdE8883655fFa370c4c4B40b76fC2F4',
+    '0xeA83A7008A2A229866f97c609446F7a3e1E1ddEc',
+    '0x224D80e3B3aCbaDf6193D2D68d05C5FFCB2ef59c',
+    '0xDfA67dc7B5c716038BB2491808d4cCDB34770f36',
+    '0x86F89c3f941fD55D210dd4Aec498DdcD8B66d829',
+    '0xE5D2800bd434C9a7D89Ae3747fddB132AFFB0035',
+    '0x160C7FbA44e124A2774Aa92d55b096f860D0B29e',
+    '0x405ED75FC41e795EA9a62D04f8007ABd5Da24AeC',
+    '0xC9079C5cD2d7BAD222c82a5e474D55d049773A5E',
+    '0xAb12A6137E0950202f54B56937EEe3da9628bb3B',
+    '0x6D9B12C2068b4E6f82F4A272893B47f24A327859',
+    '0xcb0F68f4e04742d9573fF79C0851c69C9d754195',
+    '0x074a309c4aa50227A92BD49e0cd69C2AAe5F932a',
+    '0x9D8d1a39AD998CA89d2Ec75FA29498a1E960e35C',
+    '0x71f0fBE64400D088378eca7Cee3Ab3456e88f262',
+    '0x18E990065759517fb2fb4431211cBEc5Ac54ca32',
+    '0xB08d035A5e4A57B69936501C270f4e964249dfcc',
+    '0x316646D904F433F1fFe0A008D638685f413A54C6',
+    '0xA8c3C5d834cBFE10B183eaf004A74CDFA76386E7',
+    '0xa9830D4222Df2C962021562893D1482c60403482',
+    '0x5F8BCC75B654Ada684e14e354b7976C24a632878',
+    '0x3898F055Dd754Bc1078a7D82f27B7fbf187F062C',
+    '0x42E70F4Dca2F0ba430b9C65B51136b76943dE5F9',
+    '0x1900C55727588B7c923ba5077C5d4369123715f8',
+    '0x1a8e86F1207A6Ba9E3959F70c3fF24e0c5B8aCd5',
+    '0x9a76cE8A8448293074Dd145bbC387eF778883833',
+    '0x08e5B2C3c52b92981D7E0Cb46176cE73025B0018',
+    '0xA4d95E7FFb415f73d5d79525d9777b158201361C',
+    '0x1E03E3EC229151b5a44C069E3aE7DAD36b759BB8',
+    '0xC14F9d18115cc3665480ef3873607AeF66ae4f85',
+    '0xC3AdE47C4E1bb6d36d8Ef9c8083bcb1f073EcE0b',
+    '0xeD01841D17E77e5080ba24b3C529f37fCBeF902F',
+    '0xb453046B86Bc2a7C447653E63F19608E4DBf9865',
+    '0xF8B90213b5E113d5bd9A155376A7230c2dab3B13',
+]
+
+# BaseScan API 配置
+BASESCAN_API = 'https://api.basescan.org/api'
+API_KEY = '7DDEE4CZAI6GIGR76KC5RW2CJGDTMME8W8'  # 可选：填写你的 API Key，空也可以调用
+proxies = {
+    'http': 'socks5h://888:888@38.247.10.115:10000',  # 本地代理地址和端口
+    'https': 'socks5h://888:888@38.247.10.115:10000'
+}
+
+
+def get_latest_transactions(address, limit=5000):
+    params = {
+        'module': 'account',
+        'action': 'txlist',
+        'address': address,
+        'startblock': 0,
+        'endblock': 99999999,
+        'page': 1,
+        'offset': limit,
+        'sort': 'desc',
+        'apikey': API_KEY
+    }
+
+    response = requests.get(BASESCAN_API, proxies=proxies, params=params)
+    data = response.json()
+    # print(data)
+    if data.get("status") != "1":
+        return None
+    return data["result"]
+
+
+def calculate_time_difference(start_time_str, timezone_str="Asia/Shanghai"):
+    """计算与当前时间的时间差（小时）"""
+    try:
+        tz = pytz.timezone(timezone_str)
+        start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M:%S")
+        start_time = tz.localize(start_time)
+        now = datetime.now(tz)
+        delta = now - start_time
+        hours_diff = delta.total_seconds() / 3600
+        return int(hours_diff)
+    except Exception as e:
+        return f"时间解析错误: {e}"
+
+
+def main(index, wallet):
+    MAX_RETRY = 9999  # 设置一个非常大的重试次数（或用 while True 无限循环）
+
+    # for index, wallet in enumerate(wallets):
+
+    # 自动重试获取交易
+    for attempt in range(MAX_RETRY):
+        try:
+            transactions = get_latest_transactions(wallet)
+            if not transactions:
+                pass
+            break  # 成功则跳出重试循环
+        except Exception as e:
+            # print(f"  ❌ 获取交易失败（尝试第 {attempt + 1} 次）: {e}")
+            sleep(1)  # 每次失败后等 3 秒再试
+    # 如果 transactions 为空也继续打印逻辑
+    n = 0
+    t = ""
+    list = [f"\n📬 第 {index} 个钱包地址: {wallet}"]
+    if not transactions:
+        list.append(f"    合约地址: ")
+    for i, tx in enumerate(transactions, 1):
+        try:
+            if not t:
+                t = tx
+            if tx.get('contractAddress', ''):
+                list.append(f"    合约地址: {tx['contractAddress']}")
+            if tx['to'].upper() == '0XD85EE50DA419CC5AF83A1E70A91D5C630B8C650A'.upper():
+                n += 1
+        except Exception as e:
+            print(f"     ⚠️ 处理交易失败: {e}")
+            continue
+    list.append(
+        f"    时间: {datetime.fromtimestamp(int(t['timeStamp']))} 距离现在 {calculate_time_difference(str(datetime.fromtimestamp(int(t['timeStamp']))))} 个小时没有发起验证")
+    list.append(f"    发起验证次数: {n}")
+    list.append("     =====================================================================")
+    print("\n".join(list))
+    if wallet not in walletsnum:
+        walletsnum[wallet] = n
+    else:
+        if walletsnum[wallet]!=n:
+            walletsnum[wallet] = n
+            print(f'钱包{wallet} 发钱新的验证了，总次数{n}')
+    return ""
+
+
+def process_items_main(batch_size=4):
+    """多线程处理数据，每次 batch_size 个"""
+    results = []
+    result_queue = queue.Queue()
+    b = 0
+    for i in range(0, len(wallets), batch_size):
+        batch = wallets[i:i + batch_size]
+        print(f"处理批次 {i // batch_size + 1}，任务数: {len(batch)}")
+        with ThreadPoolExecutor(max_workers=batch_size) as executor:
+            # futures = [
+            #     executor.submit(main, index, item)
+            #     for index, item in enumerate(batch)
+            # ]
+            for index, item in enumerate(batch):
+                executor.submit(main, b + 1, item)
+                b += 1
+            # 等待当前批次完成
+            # for future in futures:
+            #     try:
+            #         future.result()  # 确保捕获异常
+            #     except Exception as e:
+            #         print(f"线程任务异常: {e}")
+
+        # 收集结果
+        while not result_queue.empty():
+            results.append(result_queue.get())
+
+        time.sleep(1)  # 批次间短暂休眠，避免过载
+
+    return results
+
+
+def connect_to_server(host, username, password=None, key_path=None, port=22):
+    """建立 SSH 连接"""
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        if password:
+            ssh.connect(host, port, username, password)
+        elif key_path:
+            ssh.connect(host, port, username, key_filename=key_path)
+        print("成功连接到服务器")
+        return ssh
+    except Exception as e:
+        print(f"连接失败: {e} {host}")
+        return None
+
+
+def execute_docker_logs(ssh, container_name, ip, tail_lines=100):
+    """执行 docker logs 命令并返回结果"""
+    command = f"docker logs --tail {tail_lines} {container_name}"
+    strs = [ip]
+    list = []
+    try:
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode()
+        error = stderr.read().decode()
+        if output:
+            list.append(f"日志输出:{ip}")
+            list.append(output)
+            if 'Subscription completed' in output and 'subscription creation' in output and 'retrying in 448' not in output:
+                strs.append("地狱节点日志正常")
+            else:
+                strs.append("地狱节点日志不正常")
+        if error:
+            print("错误信息:")
+            print(error)
+    except Exception as e:
+        print(f"命令执行失败: {e}")
+
+    try:
+        command = f"docker ps -a"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode()
+        error = stderr.read().decode()
+        if output:
+            list.append(output)
+            if 'infernet-node' in output and 'infernet-anvil' in output and 'infernet-redis' in output and 'infernet-fluentbit' in output and 'hello-world' in output:
+                strs.append("地狱节点docker容器数量正常")
+            else:
+                strs.append("地狱节点docker容器数量不正常")
+            if "exited" in output.lower() or "dead" in output.lower() or "oom_killed" in output.lower() or "removing" in output.lower():
+                strs.append("地狱节点docker容器状态不正常")
+            else:
+                strs.append("地狱节点docker容器状态正常")
+        if error:
+            print("错误信息:")
+            print(error)
+    except Exception as e:
+        print(f"命令执行失败: {e}")
+    finally:
+        print("\n".join(list))
+        return strs
+
+
+ips = [
+    '38.247.10.115:8R0f5GGEruv2Cm',
+    '38.247.9.103:8R0f5GGEruv2Cm',
+    '38.247.11.87:8R0f5GGEruv2Cm',
+    '38.247.15.69:8R0f5GGEruv2Cm',
+    '38.247.9.71:80ceA1r1gftpwe',
+    '38.247.14.89:qtarBMDY6343',
+    '38.247.12.92:8R0f5GGEruv2Cm',
+    '38.247.13.67:8R0f5GGEruv2Cm',
+    '38.247.14.84:8R0f5GGEruv2Cm',
+    '38.247.14.83:nctiASER1165',
+    '38.247.8.94:8R0f5GGEruv2Cm',
+    '38.247.9.89:tvvsHGEJ4165',
+    '38.247.11.113:ddmbUJRL1511',
+    '154.202.118.31:10qyHwTurwcfZmQ',
+    '154.202.119.89:10qyHwTurwcfZmQ',
+    '154.202.119.35:10qyHwTurwcfZmQ',
+    '154.202.118.252:10qyHwTurwcfZmQ',
+    '154.202.118.114:10qyHwTurwcfZmQ',
+    '38.247.11.90:7r1L8IpEwzegYN',
+    '38.247.10.90:7r1L8IpEwzegYN',
+    '38.247.11.99:7r1L8IpEwzegYN',
+    '38.247.9.126:7r1L8IpEwzegYN',
+    '38.247.9.86:7r1L8IpEwzegYN',
+    '38.247.8.86:8QsofS2MuHh15I',
+    '38.247.8.73:8QsofS2MuHh15I',
+    '38.247.15.90:8QsofS2MuHh15I',
+    '38.247.8.82:2SBzwhMrUGG4CR',
+    '38.247.12.74:2SBzwhMrUGG4CR',
+    '38.247.9.80:2SBzwhMrUGG4CR',
+    '38.247.14.77:2SBzwhMrUGG4CR',
+    '38.247.15.85:2SBzwhMrUGG4CR',
+    '38.247.8.89:2SBzwhMrUGG4CR',
+    '38.247.12.72:1BFxA644oPbE3z',
+    '38.247.14.87:1BFxA644oPbE3z',
+    '38.247.12.78:1BFxA644oPbE3z',
+    '38.247.15.93:1BFxA644oPbE3z',
+    '38.247.13.84:2CKJqn8xf5zfbf',
+    '38.247.13.90:2CKJqn8xf5zfbf',
+    '38.247.8.114:2CKJqn8xf5zfbf',
+    '154.202.118.210:10qyHwTurwcfZmQ',
+    '38.247.12.66:1BFxA644oPbE3z',
+    '38.247.10.106:1BFxA644oPbE3z',
+    '38.247.8.124:2CKJqn8xf5zfbf',
+    '38.247.11.88:2CKJqn8xf5zfbf',
+    '38.247.15.75:2CKJqn8xf5zfbf',
+    '38.247.14.81:myzbCDVD5735',
+    '38.247.10.123:2CKJqn8xf5zfbf',
+    '156.239.40.237:xkkgATRF2869',
+]
+
+
+def process_items(batch_size=30):
+    """多线程处理数据，每次 batch_size 个"""
+    results = []
+    result_queue = queue.Queue()
+    for i in range(0, len(ips), batch_size):
+        batch = ips[i:i + batch_size]
+        print(f"处理批次 {i // batch_size + 1}，任务数: {len(batch)}")
+        with ThreadPoolExecutor(max_workers=batch_size) as executor:
+            futures = [
+                executor.submit(sship, item, result_queue)
+                for item in batch
+            ]
+            # 等待当前批次完成
+            for future in futures:
+                try:
+                    future.result()  # 确保捕获异常
+                except Exception as e:
+                    print(f"线程任务异常: {e}")
+
+        # 收集结果
+        while not result_queue.empty():
+            results.append(result_queue.get())
+
+        time.sleep(1)  # 批次间短暂休眠，避免过载
+    for v in results:
+        print(v)
+        if "地狱节点docker容器数量不正常" in "".join(v):
+            print(f"容器缺少告警，需要重新运行脚本 {v[0]}")
+            dockerrun(v[0])
+        elif "地狱节点docker容器状态不正常" in "".join(v):
+            print(f"正在重启docker")
+            dockerrun(v[0])
+        elif "地狱节点日志不正常" in "".join(v):
+            print(f"正在重启docker")
+            dockerrun(v[0])
+        print("==================================================")
+
+    return results
+
+
+def sship(ip, result_queue):
+    # list = []
+    # for ip in ips:
+    # 服务器连接信息
+    ip = ip.split(":")
+    host = ip[0]  # 替换为你的服务器 IP
+    username = "root"  # 替换为你的用户名
+    password = ip[1]  # 如果使用密码认证
+    # 服务器连接信息
+    # key_path = "/path/to/your/private/key"  # 如果使用密钥认证
+    container_name = "infernet-node"
+    tail_lines = 100
+
+    for i in range(10):
+        # 连接服务器
+        ssh = connect_to_server(host, username, password=password)
+        if not ssh:
+            # 发送邮箱
+            if i == 9:  # 发送邮箱
+                pass
+            return
+        else:
+            break
+
+    try:
+        # 执行 docker logs 命令
+        strs = execute_docker_logs(ssh, container_name, ":".join(ip), tail_lines)
+    finally:
+        # 关闭连接
+        ssh.close()
+        result_queue.put(strs)
+        print("SSH 连接已关闭")
+
+
+def dockerrun(ip):
+    # list = []
+    # for ip in ips:
+    # 服务器连接信息
+    ip = ip.split(":")
+    host = ip[0]  # 替换为你的服务器 IP
+    username = "root"  # 替换为你的用户名
+    password = ip[1]  # 如果使用密码认证
+    # 服务器连接信息
+    # key_path = "/path/to/your/private/key"  # 如果使用密钥认证
+    container_name = "infernet-node"
+    tail_lines = 100
+
+    # 连接服务器
+    ssh = connect_to_server(host, username, password=password)
+    if not ssh:
+        return
+    try:
+        """执行 docker logs 命令并返回结果"""
+        command = f"docker compose -f /root/infernet-container-starter/deploy/docker-compose.yaml down&&docker compose -f /root/infernet-container-starter/deploy/docker-compose.yaml up -d"
+        strs = [ip]
+        list = []
+        try:
+            stdin, stdout, stderr = ssh.exec_command(command)
+            output = stdout.read().decode()
+            error = stderr.read().decode()
+            if output:
+                list.append(f"日志输出:{ip}")
+                list.append(output)
+            if error:
+                print("错误信息:")
+                print(error)
+        except Exception as e:
+            print(f"命令执行失败: {e}")
+    finally:
+        # 关闭连接
+        print("\n".join(list))
+        ssh.close()
+        print("SSH 连接已关闭")
+
+
+if __name__ == "__main__":
+    pass
+    while True:
+        try:
+            # 检测钱包验证
+            process_items_main()
+
+            # 检测服务器运行
+            process_items()
+
+        finally:
+            print('等待10分钟再次运行')
+            time.sleep(60*10)
