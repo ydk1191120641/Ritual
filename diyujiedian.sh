73089732662afed7b7974ed75c1f2afb0af7e595
ydk1191120641 <66811213+ydk1191120641@users.noreply.github.com>
2025-04-22 18:28:31 +0800

    Update diyujiedian.sh

--- diyujiedian.sh
+++ diyujiedian.sh
@@ -34,39 +34,6 @@ clean_json_files() {
         # 记录错误
         echo "${timestamp}--删除失败" >> $LOG_FILE
     fi
-
-    # 提取 MergedDir
-    MERGED_DIR=$(docker inspect infernet-anvil | jq -r '.[0].GraphDriver.Data.MergedDir')
-
-    if [ -z "$MERGED_DIR" ]; then
-        echo "无法获取 MergedDir 路径"
-        exit 1
-    fi
-
-    echo "找到 MergedDir: $MERGED_DIR"
-
-    # 2. 确定目标
-    TARGET_DIR="${MERGED_DIR}/root/.foundry/anvil/tmp"
-
-    if [ ! -d "$TARGET_DIR" ]; then
-        echo "目标目录不存在: $TARGET_DIR"
-        exit 1
-    fi
-
-    echo "正在处理目录: $TARGET_DIR"
-
-    # 3. 重命名子文件夹
-    for folder in "$TARGET_DIR"/*; do
-        if [ -d "$folder" ]; then
-            # 生成随机字符串
-            NEW_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
-            PARENT_DIR=$(dirname "$folder")
-            mv "$folder" "$PARENT_DIR/$NEW_NAME"
-            echo "将 $(basename "$folder") 重命名为 $NEW_NAME"
-        fi
-    done
-
-    echo "所有文件夹已随机重命名"
 }
 
 # 立即执行一次
